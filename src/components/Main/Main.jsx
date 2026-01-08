3eb5b7f7466d67e61a55f02df7afa0fb9b34d37e
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-08 10:57:25 +0530

    added cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,30 @@ const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon} alt="" />
         </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Anshul</span></p>
+                <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Plan a holiday</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Think a new idea</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Talk to me</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Help me with this code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
